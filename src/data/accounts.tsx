2c41ef9a616117779e00a90f52cf42cbcc23cfb3
Erik Nielsen <erik@312development.com>
2024-04-28 21:46:15 -0400

    updated readme

--- src/data/accounts.tsx
+++ src/data/accounts.tsx
@@ -11,12 +11,6 @@ const accounts = [
     interest: { value: 4.75 },
     minPayment: { value: 150 },
   },
-  {
-    name: { value: "Another Loan" },
-    balance: { value: 4000 },
-    interest: { value: 10 },
-    minPayment: { value: 125 },
-  },
 ];
 
 export default accounts;
